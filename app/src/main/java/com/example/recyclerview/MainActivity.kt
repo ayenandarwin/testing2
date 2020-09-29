17395488942af4865def49b2abdc4f28a308c6d8
Aye Nandar Win <ayenandarwin725@gmail.com>
2020-09-29 08:51:57 +0630

    Simple recyclerview

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.adapter.FlowerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,7 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         var flowerAdapter = FlowerAdapter(flowerList)
         flowerRecycler.layoutManager = LinearLayoutManager(this)
+        flowerRecycler.layoutManager=LinearLayoutManager(
+         this,LinearLayoutManager.VERTICAL,false)
+
+        //flowerRecycler.layoutManager =GridLayoutManager(this,2)
         flowerRecycler.adapter = flowerAdapter
+
 
     }
 }
